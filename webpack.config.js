90055a639f47ac7d8ab1257e412b05575ad82089
Tyrice Clark <tyricec@civicresource.com>
2017-04-29 22:49:58 -0700

    chore(*): Change devserver path to /dist/

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path')
 
 const config = {
+  devServer: {
+    publicPath: '/dist/'
+  },
   devtool: 'cheap-module-eval-source-map',
   entry: path.resolve(__dirname, 'main.js'),
   output: {

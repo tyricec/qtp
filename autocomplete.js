8079f79c586add68d63bb1cb51d832178983cbc1
Tyrice Clark <tyricec@civicresource.com>
2017-04-29 21:50:11 -0700

    feat(*): Position autocomplete underneath input

--- autocomplete.js
+++ autocomplete.js
@@ -1,12 +1,16 @@
 const autocomplete = {
   attach: function(input) {
-    var element = document.createElement('div')
-    var list = document.createElement('ul')
+    const element = document.createElement('div')
+    const list = document.createElement('ul')
+    const boundingRect = input.getBoundingClientRect()
 
     element.className = 'qtp-autocomplete'
     list.className = 'qtp-autocomplete__list'
 
     element.appendChild(list)
+
+    element.style.top = boundingRect.bottom + 'px'
+    element.style.left = boundingRect.left + 'px'
 
     document.body.appendChild(element)
   }

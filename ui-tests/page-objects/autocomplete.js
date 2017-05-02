8545a27d6b42d63fb27f6506334dde43da640362
Tyrice Clark <tyricec@civicresource.com>
2017-05-01 22:42:31 -0700

    chore(*): Add page object config

---
+++ ui-tests/page-objects/autocomplete.js
@@ -0,0 +1,7 @@
+module.exports = {
+  elements: {
+    autocomplete: {
+      selector: '.qtp-autocomplete',
+    },
+  },
+}
---
+++ ui-tests/tests/autocomplete.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'Autocomplete is Present': function (browser) {
+    var autocomplete = browser.page.autocomplete()
+
+    browser
+      .url(browser.launchUrl + ':8080')
+      .waitForElementVisible('#qtp-start-point', 2000)
+      .setValue('#qtp-start-point', '12041 Brixton')
+
+    autocomplete.expect.element('@autocomplete').to.be.present.before(2000)
+  },
+}

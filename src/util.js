1445e852fa226eeeed2a0bd18b0a8d3a9d029467
Tyrice Clark <tyricec@civicresource.com>
2017-05-03 00:23:19 -0700

    feat(*): Add buildHTML and buildText utility functions

--- src/util.js
+++ src/util.js
@@ -1,7 +1,25 @@
 const util = {
-  createElement: function(type) {
-    const element = document.createElement(type)
+  buildHTML: (type, attrs, inner) => {
+    let element
+
+    if (type) {
+      element = document.createElement(type)
+    }
+    
+    if (element && attrs) {
+      Object.keys(attrs).forEach((attr) => {
+        element.setAttribute(attr, attrs[attr])
+      })
+    }
+
+    if (element && inner) {
+      element.appendChild(inner)
+    }
+
     return element
+  },
+  buildText: (text) => {
+    return document.createTextNode(text)
   },
 }
 
--- src/util.spec.js
+++ src/util.spec.js
@@ -1,13 +1,43 @@
 import util from './util'
 
-test('createElement creates div element', () => {
-  const result = util.createElement('div')
+test('buildHTML creates div element', () => {
+  const result = util.buildHTML('div')
 
   expect(result.outerHTML).toMatchSnapshot()
 })
 
-test('createElement create li element', () => {
-  const result = util.createElement('li')
+test('buildHTML create li element', () => {
+  const result = util.buildHTML('li')
 
   expect(result.outerHTML).toMatchSnapshot()
+})
+
+test('buildHTML adds attribues', () => {
+  const result = util.buildHTML(
+    'li',
+    {
+      class: 'list-item',
+      style: 'top: 20px;color: white;left: 5px',
+    }
+  )
+
+  expect(result.outerHTML).toMatchSnapshot()
+})
+
+test('buildHTML adds text', () => {
+  const result = util.buildHTML(
+    'div',
+    {},
+    document.createTextNode('Build HTML Test')
+  )
+
+  expect(result.outerHTML).toMatchSnapshot()
+})
+
+test('buildText creates text node', () => {
+  const result = util.buildText(
+    'Build Text Test'
+  )
+
+  expect(result.wholeText).toMatchSnapshot()
 })

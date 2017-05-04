c110efab62ab2dd61507fbcdb5e1726f62c23cc0
Tyrice Clark <tyricec@civicresource.com>
2017-05-04 01:45:50 -0700

    feat(*): Add autocomplete list render method

---
+++ src/renderPredictionList.js
@@ -0,0 +1,27 @@
+import util from './util'
+
+function renderPredictionList(results) {
+  const [predictions, status,] = results
+
+  if (status === 'OK') {
+    return predictions.map((prediction) => {
+      const listItem = util.buildHTML('li', {
+        class: 'qtp-autocomplete__list-item',
+      }, util.buildText(prediction.description))
+
+      listItem.appendChild(util.buildHTML('hr', {
+        class: 'qtp-autocomplete__hr',
+      }))
+
+      return listItem
+    }).reduce((acc, current) => {
+      acc.appendChild(current)
+      return acc
+    }, util.buildHTML(
+      'div', {
+        class: 'qtp-autocomplete',
+      }, util.buildHTML('ul', { class: 'qtp-autocomplete__list', })))
+  }
+}
+
+export default renderPredictionList

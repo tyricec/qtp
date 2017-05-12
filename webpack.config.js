ee3a376f687e07abbaa52155fb517a1656646d28
Tyrice Clark <tyricec@civicresource.com>
2017-05-12 01:20:49 -0700

    style(*): Update mobile style

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const path = require('path')
 const config = {
   devServer: {
     publicPath: '/dist/',
+    host: '0.0.0.0',
+    disableHostCheck: true,
   },
   devtool: 'cheap-module-eval-source-map',
   entry: path.resolve(`${__dirname}/src`, 'main.js'),
